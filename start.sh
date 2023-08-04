9136dedac3feed7062e1dfca475b180307da8686
Replit user <>
2023-08-04 08:28:05 +0000

    nginx config

---
+++ start.sh
@@ -0,0 +1,12 @@
+# git pull origin master
+echo "START"
+echo
+{
+	nginx -p /home/runner/$REPL_SLUG/ -e logs/nginx.log -c resources/nginx/nginx.conf -g 'daemon off;'
+} &
+{
+	cd backend/ && npm run start
+} & 
+{
+	cd frontend/ && npm run start
+}
